3a396593c7a68e6289dd06149089e14bea197d9c
QUINCY HUTCHISON <30450645+nativeB@users.noreply.github.com>
2019-07-08 23:43:51 +0000

    switch to es6 class

--- index.js
+++ index.js
@@ -1,102 +1,140 @@
 
-const fs = require('fs');
+const fs= require('fs');
 const path = require('path');
 const changeCase = require('change-case')
 const replace = require('replace-in-file');
-// let changeCase = require('change-case')
-let walkPath = './components';
-const config = {
-    case:'snake',
-    level:'sub',//sub
-    findandreplaceusage:true,
-    replaceCasesInFiles:false,
-    ignore:['node_modules','gitkeep']
-}
 
-let renamed=[]
-let fileList=[]
-function getStats(path,name) {
-    let stat = fs.statSync(path+'/'+name)
-    stat.path=path
-    stat.name=name
-    return stat
-}
-//using synchronous fns so we can build a full list including nested files
-function walk (dir) {
-    let list = fs.readdirSync(dir)
-    for (let i = 0; i < list.length; i++) {
-        // console.log(list[i])
-        let stat = getStats(dir, list[i])
-        if (stat && stat.isDirectory() && config.level !== 'top') {
-            walk(stat.path + '/' + stat.name);
-        } else {
+class Pacman {
+    constructor(options){
+        this.options = options
+        this.fileList = []
+        this.innerDirList = []
+        if(this.options.renameFiles){
+            this.renameFiles(null,this.buildDirectory(this.options.path,null,0),null, null)
+        }
+    }
+    //resorting to sync fns
+    //be using glob soon
+    buildDirectory(path,cb,level){
+        level++
+        // console.log(level)
+        try {
+           let dir = fs.readdirSync(path)
+           // if(typeof  cb === 'function')cb(dir)
+           if(this.options.deep) {
+               for (let i = 0; i < dir.length; i++) {
+                   let stat = this.getStats(path, dir[i])
+                       if (stat && stat.isDirectory()) {
+                       // console.log('found')
+                            this.innerDirList.push(stat.dir + '/' + stat.name)
+                           this.buildDirectory(stat.dir + '/' + stat.name, null, level)
+                       } else {
+                            this.fileList.push({dirPath:stat.dir,fileName: stat.name})
+                           // cb?cb(stat.dir, stat.name) : Pacman.log('found file', dir[i], 'in', stat.dir,level)
 
-            let filePath = stat.path + '/' + stat.name
-            let fileName = path.basename(stat.name, path.extname(stat.name))
-            let extention = path.extname(stat.name)
-            if (!stat.name && config.ignore.includes(stat.name)) {
-                return
-            }
-            let newFileName = ''
-            let toPath = ''
+                       }
 
-            switch (config.case) {
+               }
+           }
+            level--
+           if(level===0) {
+               return cb?cb(this.fileList): this.fileList
+           }
+        }catch(e) {
+            return Pacman.err(e)
+       }
+
+                   // if (stat && stat.isDirectory() && config.level !== 'top') {
+                   //     walk(stat.path + '/' + stat.name);
+                   // } else {
+                   //     this.dirList.push(stat.path + '/' + stat.name)
+                   // }
+               // }
+           // }
+       // })
+    }
+    renameFiles(casetouse,files,dirpath,cb){
+        // if(!files)
+        let filesList=files||this.buildDirectory(this.options.path||dirpath,null,0)
+        let newFileName = ''
+        let toPath = ''
+        let fromPath, fileName, extention
+        filesList.forEach(file =>{
+            fromPath= file.dirPath + '/' + file.fileName
+            fileName = path.basename(file.fileName, path.extname(file.fileName))
+            extention = path.extname(file.fileName)
+            let casing = casetouse||this.options.case
+            // console.log(casing)
+            switch (casing) {
                 case 'camel':
                     newFileName = changeCase.camelCase(fileName)
 
                     break
                 case 'snake':
                     newFileName = changeCase.snakeCase(fileName)
-
+                    break
+                default:
+                    // console.log('error')
                     break
             }
-            toPath = path.join(stat.path, newFileName + extention);
-            fs.renameSync(filePath, toPath)
+            if(newFileName) {
+                toPath = path.join(file.dirPath, newFileName + extention);
+                // console.log(toPath)
+                fs.renameSync(fromPath, toPath)
+                file.toPath= toPath
+                file.newFileName=newFileName
+                file.fileName=fileName
+                file.extention=extention
+                file.case=this.options.case
+            }
 
-            renamed.push({oldName: fileName, newName: newFileName})
-            fileList.push(toPath)
-            //find occurrences of renamed in files if only replace filenames is opted
-            console.log("Renamed file '%s' to '%s'.", stat.name, newFileName);
+        })
+        if(this.options.replaceInFiles)
+            this.replaceInFiles(filesList)
+        else if(this.options.cb) this.options.cb(filesList)
+        else if(cb) cb()
+        else Pacman.log(filesList)
 
-
-        }
     }
-}
-
-function replaceInFiles() {
-    console.log(renamed)
-    replace({
-        files: fileList,
-        from: renamed.map(names => new RegExp(names.oldName,'g')),
-        to: renamed.map(names => names.newName),
-        countMatches:true
-    }).then(results => {
-        console.log('Replacement results:', results);
-    }).catch(error => {
+    //prefer to use promise here
+    replaceInFiles(fileList,cb) {
+        console.log(fileList)
+        replace({
+            files: fileList.map(file=> file.toPath),
+            from: fileList.map(file => new RegExp(file.fileName,'g')),
+            to: fileList.map(file => file.newFileName),
+            countMatches:true
+        }).then(results => {
+            for(let i = 0; i <results.length;i++)fileList[i]={...fileList[i],...results[i]}
+        if(this.options.cb) this.options.cb(fileList)
+            else if(cb) cb()
+        }).catch(error => {
             console.error('Error occurred:', error);
         });
+    }
+    replaceOccurrencesInFiles(){
+
+    }
+    logError(){
+
+    }
+    static log(){
+    console.log(...arguments)
+    }
+    getStats(dir,name){
+        try {
+            let stat = fs.statSync(dir + '/' + name)
+            stat.dir =dir
+            stat.name=name
+            return stat
+        }catch(e){
+            Pacman.err(e)
+        }
+    }
+    static err(err){
+        throw new Error(err)
+    }
+
 }
-// optional command line params
-//      source for walk path
-// process.argv.forEach(function (val, index, array) {
-//     if (val.indexOf('source') !== -1) {
-//         walkPath = val.split('=')[1];
-//     }
-// });
 
-console.log('-------------------------------------------------------------');
-console.log('processing...');
-console.log('-------------------------------------------------------------');
-
-walk(walkPath);
-console.log('-------------------------------------------------------------');
-console.log('done renaming files...');
-console.log('-------------------------------------------------------------');
-
-if(config.findandreplaceusage) {
-    console.log('-------------------------------------------------------------');
-    console.log('renaming in files...');
-    console.log('-------------------------------------------------------------');
-
-    replaceInFiles()
-}
+// let pac= new Pacman({path:'./components',deep:true,renameFiles:true,case:'camel',replaceInFiles:true,cb:(files)=>{console.log('this renamed',files)}})
